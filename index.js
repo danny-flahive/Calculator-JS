ed938ca8336e240892f0b41dc62b4066dda965d9
danny-flahive <93837720+danny-flahive@users.noreply.github.com>
2021-11-10 16:11:39 +0000

    Ex 3 Takes an operator as input
    Takes an operator as well as two integer inputs. The operator input is properly validated so the switch statement doesn't run into any problems.

--- index.js
+++ index.js
@@ -2,10 +2,34 @@ const readLine = require('readline-sync');
 console.log('Welcome to the Calculator');
 console.log('=========================');
 
+validOperator = false;
+operator = "";
+while (!validOperator) {
+    if (operator != "+" && operator != "-" && operator != "/" && operator != "*") {
+        console.log("Enter an operator (+, -, *, /)");
+        operator = readLine.prompt();
+    }
+    else {
+        validOperator = true;
+    }
+}
 console.log('Enter the first number: ');
-var firstNum = readLine.prompt();
+var firstNum = parseInt(readLine.prompt());
 console.log('Enter the second number: ');
-var secondNum = readLine.prompt();
+var secondNum = parseInt(readLine.prompt());
 
-var result = parseInt(firstNum) * parseInt(secondNum);
+var result;
+switch (operator) {
+    case "+":
+        result = firstNum + secondNum;
+        break;
+    case "-":
+        result = firstNum - secondNum;
+        break;
+    case "*":
+        result = firstNum * secondNum;
+        break;
+    case "/":
+        result = firstNum / secondNum;
+}
 console.log("The result is: " + result);
