0f1c11b893658f9930cc11c3c2e6919a70a29f94
danny-flahive <93837720+danny-flahive@users.noreply.github.com>
2021-11-10 15:45:52 +0000

    Ex 2. Added a simple multiplier

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+const readLine = require('readline-sync');
 console.log('Welcome to the Calculator');
+console.log('=========================');
+
+console.log('Enter the first number: ');
+var firstNum = readLine.prompt();
+console.log('Enter the second number: ');
+var secondNum = readLine.prompt();
+
+var result = parseInt(firstNum) * parseInt(secondNum);
+console.log("The result is: " + result);
