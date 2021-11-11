10e680d7b7dfb6f78a05b59900b75ca773ae68cf
danny-flahive <93837720+danny-flahive@users.noreply.github.com>
2021-11-11 11:56:42 +0000

    Ex. 8 Updated arithmetic calculator
    Used array methods instead of for loops to compute results

--- arithmetic.js
+++ arithmetic.js
@@ -4,22 +4,22 @@ exports.PerformCalculation = function() {
     var operator = inputParser.GetOperator()
     var numberOfValues = inputParser.GetNumberInput('How many numbers do you want to enter? ');
     var values = inputParser.GetNumberArray(numberOfValues);
-    var result;
-    result = values[0];
-    for (let i = 1; i < values.length; i++) {
-        switch (operator) {
-            case "+":
-                result += values[i];
-                break;
-            case "-":
-                result -= values[i];
-                break;
-            case "*":
-                result *= values[i];
-                break;
-            case "/":
-                result /= values[i];
-        }
+    var result = values[0];
+    values = values.slice(1, values.length);
+    switch (operator) {
+        case "+":
+            result = values.reduce(function(accumulator, current) {return accumulator += current}, result)
+            break;
+        case "-":
+            result = values.reduce(function(accumulator, current) {return accumulator -= current}, result)
+            break;
+        case "*":
+            result = values.reduce(function(accumulator, current) {return accumulator *= current}, result)
+            break;
+        case "/":
+            values = values.filter(function (value) {return value != 0})
+            result = values.reduce(function(accumulator, current) {return accumulator /= current}, result)
+            break;
     }
     console.log("The result is: " + result);
 }
