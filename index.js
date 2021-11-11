6503fe665b9eaf12d2fc9be40c6314288f4ac079
danny-flahive <93837720+danny-flahive@users.noreply.github.com>
2021-11-11 08:57:52 +0000

    Ex 4. Can now take a specified number of operands
    Added for loops to allow the calculator to work with any number of inputs, specified by the user during execution.

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const readLine = require('readline-sync');
 console.log('Welcome to the Calculator');
 console.log('=========================');
 
-validOperator = false;
-operator = "";
-while (!validOperator) {
+var validOperator = false;
+var operator;
+do {
     if (operator != "+" && operator != "-" && operator != "/" && operator != "*") {
         console.log("Enter an operator (+, -, *, /)");
         operator = readLine.prompt();
@@ -12,24 +12,32 @@ while (!validOperator) {
     else {
         validOperator = true;
     }
+} while (!validOperator);
+
+console.log("How many numbers do you want to enter? ");
+var numberOfValues = parseInt(readLine.prompt());
+var values = []
+for (let i = 0; i < numberOfValues; i++) {
+    console.log('Enter number ' + (i + 1) + ': ');
+    values.push(parseInt(readLine.prompt()));
 }
-console.log('Enter the first number: ');
-var firstNum = parseInt(readLine.prompt());
-console.log('Enter the second number: ');
-var secondNum = parseInt(readLine.prompt());
 
 var result;
-switch (operator) {
-    case "+":
-        result = firstNum + secondNum;
-        break;
-    case "-":
-        result = firstNum - secondNum;
-        break;
-    case "*":
-        result = firstNum * secondNum;
-        break;
-    case "/":
-        result = firstNum / secondNum;
+result = values[0];
+for (let i = 1; i < values.length; i++) {
+    switch (operator) {
+        case "+":
+            result += values[i];
+            break;
+        case "-":
+            result -= values[i];
+            break;
+        case "*":
+            result *= values[i];
+            break;
+        case "/":
+            result /= values[i];
+    }
 }
+
 console.log("The result is: " + result);
