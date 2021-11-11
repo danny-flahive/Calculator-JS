e1d2255fdc684a728d28553c2928b27a5358188d
danny-flahive <93837720+danny-flahive@users.noreply.github.com>
2021-11-11 09:43:35 +0000

    Ex 5. Refactored code into multiple functions
    Made code easier to read by splitting it into functions. Also added validation for all numeric input.

--- index.js
+++ index.js
@@ -1,43 +1,65 @@
 const readLine = require('readline-sync');
-console.log('Welcome to the Calculator');
-console.log('=========================');
 
-var validOperator = false;
-var operator;
-do {
-    if (operator != "+" && operator != "-" && operator != "/" && operator != "*") {
-        console.log("Enter an operator (+, -, *, /)");
-        operator = readLine.prompt();
-    }
-    else {
-        validOperator = true;
-    }
-} while (!validOperator);
+PrintWelcome();
+PerformCalculation();
 
-console.log("How many numbers do you want to enter? ");
-var numberOfValues = parseInt(readLine.prompt());
-var values = []
-for (let i = 0; i < numberOfValues; i++) {
-    console.log('Enter number ' + (i + 1) + ': ');
-    values.push(parseInt(readLine.prompt()));
+function PerformCalculation() {
+    var operator = GetOperator()
+    var numberOfValues = GetIntInput('How many numbers do you want to enter? ');
+    var values = GetIntArr(numberOfValues);
+    var result;
+    result = values[0];
+    for (let i = 1; i < values.length; i++) {
+        switch (operator) {
+            case "+":
+                result += values[i];
+                break;
+            case "-":
+                result -= values[i];
+                break;
+            case "*":
+                result *= values[i];
+                break;
+            case "/":
+                result /= values[i];
+        }
+    }
+    console.log("The result is: " + result);
 }
 
-var result;
-result = values[0];
-for (let i = 1; i < values.length; i++) {
-    switch (operator) {
-        case "+":
-            result += values[i];
-            break;
-        case "-":
-            result -= values[i];
-            break;
-        case "*":
-            result *= values[i];
-            break;
-        case "/":
-            result /= values[i];
-    }
+function GetIntInput(message) {
+    var value;
+    do {
+        console.log(message);
+        value = parseInt(readLine.prompt());
+    } while (isNaN(value));
+    return value;
 }
 
-console.log("The result is: " + result);
+function GetIntArr(numberOfValues) {
+    var values = [];
+    for (let i = 0; i < numberOfValues; i++) {
+        values.push(GetIntInput('Enter number ' + (i + 1) + ': '));
+    }
+    return values;
+}
+
+function GetOperator() {
+    var validOperator = false;
+    var operator;
+    do {
+        if (operator != "+" && operator != "-" && operator != "/" && operator != "*") {
+            console.log("Enter an operator (+, -, *, /)");
+            operator = readLine.prompt();
+        }
+        else {
+            validOperator = true;
+        }
+    } while (!validOperator);
+    return operator;
+}
+
+function PrintWelcome() {
+    console.log('Welcome to the Calculator');
+    console.log('=========================');
+}
